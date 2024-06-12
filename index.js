4b685e6e9aede18bc9ea495ce27cf0a1d4a84d04
Bad16dev <71897530+Bad16dev@users.noreply.github.com>
2024-06-12 19:31:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require("express")
+const app = express();
+
+app.use(express.static('public'))
+
+const PORT = process.env.PORT | 8000
+
+app.listen(PORT, () => console.log(`Sever running in ${PORT}`))
+
 const afk = require('afkbybadro');
